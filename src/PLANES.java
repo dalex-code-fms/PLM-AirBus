55048eb533fbc0525ff6ca6c0665dcb0dc2c5a38
Ferreira-RodriguesD <david.ferreira-rodrigues@fms-ea.com>
2024-09-18 11:49:37 +0200

    created a method showListOfAirPlanes and started working on user interface

--- src/PLANES.java
+++ src/PLANES.java
@@ -1,25 +1,68 @@
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Scanner;
 import java.util.UUID;
 
 public class PLANES {
 
 	public static void main(String[] args) {
-		String[][] listOfAirPlanes = getListOfAirPlanes();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Welcome to AirBus !");
+		int userChoice = 0;
 		
-		for (int l = 0; l < listOfAirPlanes.length; l++) {
 		
-			System.out.println(Arrays.toString(listOfAirPlanes[l]));				
+		while(userChoice != 5) {
+			System.out.println("\nWhat do you want to do ?\n");
+			System.out.println("1 - Show all airplanes | 2 - Search for airplane | 3 - add new piece to airplane | 4 - remove piece from airplane | 5 - exit\n");
 			
+			userChoice = sc.nextInt();
 			
+			switch (userChoice) {
+			case 1:
+				System.out.println("List of all Airplanes\n");
+				showListOfAirplanes(getListOfAirPlanes());
+				System.out.println("1 - add new piece to airplane | 2 - remove a piece from airplane | 6 - go back");
+				while (userChoice != 6) {
+					userChoice = sc.nextInt();
+					
+					if (userChoice == 1) {
+						System.out.println("add new piece to airplane");
+					} else if (userChoice == 2) {
+						System.out.println("remove a piece from airplane");
+					}
+				}
+				break;
+			case 2:
+				System.out.println("Search for airplane");
+				break;
+			case 3:
+				System.out.println("add new piece to airplane");
+				break;
+			case 4:
+				System.out.println("remove piece from airplane");
+				break;
+			default:
+				System.out.println("You have to choice at least one valid option !");
+			}
+		}
+		System.out.println("Goodbye!");
+		
+		/*for (int l = 0; l < listOfAirPlanes.length; l++) {
+			System.out.println(Arrays.toString(listOfAirPlanes[l]));					
+		}*/
+	}
+	
+	public static void showListOfAirplanes(String[][] listOfAirPlanes ) {
+		for (int i = 0; i < listOfAirPlanes.length; i++) {
+			System.out.println(Arrays.toString(listOfAirPlanes[i]));
 		}
 	}
 	
+	// generate a random UUID with 128bits and convert it to String
 	public static String createUniqueID() {
 		return UUID.randomUUID().toString();
 	}
 	
+	// list of airplanes
 	public static String[][] getListOfAirPlanes(){
 		String [][] listOfAirPlanes = {
 				{createUniqueID(), "A220-100", "conception", "transport de passagers"},
