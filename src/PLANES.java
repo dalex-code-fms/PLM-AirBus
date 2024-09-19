6d388729d4bfc889186acbf0e0a3e22ca2bfdf19
Ferreira-RodriguesD <david.ferreira-rodrigues@fms-ea.com>
2024-09-19 10:02:02 +0200

    added method addPieceToAirplane

--- src/PLANES.java
+++ src/PLANES.java
@@ -1,14 +1,19 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 import java.util.UUID;
 
 public class PLANES {
 
 	public static void main(String[] args) {
+		ArrayList<ArrayList<String>> listOfAirPlanes = getListOfAirPlanes();
+		HashMap<String, ArrayList<String>> arrayOfAirplanesPieces = new HashMap<>();
+		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Welcome to AirBus !");
+		System.out.println("Welcome to AIRBUS AIRPLANE SYSTEM!");
 		int userChoice = 0;
+		String airplaneID = "";
 			
 		while(userChoice != 5) {
 			System.out.println("\nWhat do you want to do ?\n");
@@ -19,26 +24,48 @@ public class PLANES {
 			switch (userChoice) {
 			case 1:
 				System.out.println("List of all Airplanes\n");
-				showListOfAirplanes(getListOfAirPlanes());
-				System.out.println("1 - add new piece to airplane | 2 - remove a piece from airplane | 6 - go back");
+				showListOfAirplanes(listOfAirPlanes);
+				System.out.println("1 - add new piece to airplane by ID | 2 - remove a piece from airplane by ID | 6 - go back");
 				while (userChoice != 6) {
 					userChoice = sc.nextInt();
 					if (userChoice == 1) {
-						System.out.println("add new piece to airplane");
+						System.out.println("add airplane ID to add a piece: ");
+						airplaneID = searchForAirplane(listOfAirPlanes);
+						System.out.println("1 - add new piece to airplane by ID | 2 - remove a piece from airplane by ID | 6 - go back");
+						addPieceToAirplane(airplaneID);
 					} else if (userChoice == 2) {
-						System.out.println("remove a piece from airplane");
+						System.out.println("add airplane ID to remove a piece:");
+						searchForAirplane(listOfAirPlanes);
+						System.out.println("1 - add new piece to airplane by ID | 2 - remove a piece from airplane by ID | 6 - go back");
 					}
 				}
 				break;
 			case 2:
 				System.out.println("Search for airplane");
-				searchForAirplane(getListOfAirPlanes());
+				searchForAirplane(listOfAirPlanes);
+				System.out.println("1 - add new piece to airplane | 2 - remove a piece from airplane | 6 - go back");
+				while (userChoice != 6) {
+					userChoice = sc.nextInt();
+					if (userChoice == 1) {
+						System.out.println("add airplane ID to add a piece: ");
+						airplaneID = searchForAirplane(listOfAirPlanes);
+						System.out.println("1 - add new piece to airplane by ID | 2 - remove a piece from airplane by ID | 6 - go back");
+						addPieceToAirplane(airplaneID);
+					} else if (userChoice == 2) {
+						System.out.println("add airplane ID to remove a piece: ");
+						searchForAirplane(listOfAirPlanes);
+						System.out.println("1 - add new piece to airplane by ID | 2 - remove a piece from airplane by ID | 6 - go back");
+					}
+				}
 				break;
 			case 3:
-				System.out.println("add new piece to airplane");
+				
+				System.out.println("add new piece to airplane by ID");
+				airplaneID = searchForAirplane(listOfAirPlanes);
+				addPieceToAirplane(airplaneID);
 				break;
 			case 4:
-				System.out.println("remove piece from airplane");
+				System.out.println("remove a piece from airplane by ID");
 				break;
 			default:
 				System.out.println("You have to choice at least one valid option !");
@@ -49,18 +76,50 @@ public class PLANES {
 		sc.close();
 	}
 	
-	public static void searchForAirplane(ArrayList<ArrayList<String>> listOfAirPlanes) {
+	public static void addPieceToAirplane(String airplaneID) {
+		HashMap<String, ArrayList<String>> arrayOfAirplanesPieces = new HashMap<>();
 		Scanner sc = new Scanner(System.in);
-		String userInput = sc.nextLine();
-		int airPlaneProgramme = 1;
+		String name = "";
+		String price = "";
+		String category = "";
+		System.out.println("Add the piece details for airplane : " + airplaneID);
+		System.out.print("Name : ");
+		name = sc.nextLine();
+		System.out.print("Price : ");
+		price = sc.nextLine();
+		System.out.print("Category : ");
+		category = sc.nextLine();
+		arrayOfAirplanesPieces.put(airplaneID, new ArrayList<String>(Arrays.asList("Name : " + name, "Price : " + price, "Category : " + category)));
+		System.out.println(arrayOfAirplanesPieces);
+	}
+	
+	public static ArrayList<ArrayList<String>> getArrayBySearchMethod(int index, ArrayList<ArrayList<String>> listOfAirPlanes, String userInput){
+		
 		ArrayList<ArrayList<String>> arrOfSelectedAirplanes = new ArrayList<>();
 		
 		for (int i = 0; i < listOfAirPlanes.size(); i++) {
-			if (listOfAirPlanes.get(i).get(airPlaneProgramme).contains(userInput)) {
+			if (listOfAirPlanes.get(i).get(index).contains(userInput)) {
 				arrOfSelectedAirplanes.add(listOfAirPlanes.get(i));
 			}
 		}
-		showListOfAirplanes(arrOfSelectedAirplanes);
+		return arrOfSelectedAirplanes;
+	}
+	
+	public static String searchForAirplane(ArrayList<ArrayList<String>> listOfAirPlanes) {
+		Scanner sc = new Scanner(System.in);
+		String userInput = sc.nextLine();
+		String airplaneID = "";
+		
+		int sizeOfID = createUniqueID().length();
+
+		if (userInput.length() == sizeOfID) {
+			showListOfAirplanes(getArrayBySearchMethod(0, listOfAirPlanes, userInput));
+			airplaneID = userInput;
+		} else {
+			showListOfAirplanes(getArrayBySearchMethod(1, listOfAirPlanes, userInput));
+		}
+		
+		return airplaneID;
 	}
 	
 	public static void showListOfAirplanes(ArrayList<ArrayList<String>> listOfAirPlanes) {
@@ -97,12 +156,15 @@ public class PLANES {
 		listOfAirPlanes.add(new ArrayList<>(Arrays.asList(createUniqueID(), "C295", "conception", "militaire")));
 		listOfAirPlanes.add(new ArrayList<>(Arrays.asList(createUniqueID(), "C295", "conception", "militaire")));
 		listOfAirPlanes.add(new ArrayList<>(Arrays.asList(createUniqueID(), "A330 MRTT", "conception", "militaire")));
-		listOfAirPlanes.add(new ArrayList<>(Arrays.asList(createUniqueID(),"ACJ320neo", "conception", "affaires")));
+		listOfAirPlanes.add(new ArrayList<>(Arrays.asList(createUniqueID(), "ACJ320neo", "conception", "affaires")));
 		listOfAirPlanes.add(new ArrayList<>(Arrays.asList(createUniqueID(), "ACJ318 Atlas", "conception", "affaires")));
 		listOfAirPlanes.add(new ArrayList<>(Arrays.asList(createUniqueID(), "ACJ350 XWB", "conception", "affaires")));
 		listOfAirPlanes.add(new ArrayList<>(Arrays.asList(createUniqueID(), "ACJ318 Atlas", "conception", "affaires")));
 		listOfAirPlanes.add(new ArrayList<>(Arrays.asList(createUniqueID(), "ACJ320neo", "conception", "affaires")));
 		listOfAirPlanes.add(new ArrayList<>(Arrays.asList(createUniqueID(), "ACJ350 XWB", "conception", "affaires")));
+		
+		// retirer le 2eme ArrayList et faire un array normal ArrayList<String []>
+		// pour les pieces peut etre un arrayList of arrays et puis concat si id ==
 		
 		return listOfAirPlanes;
 	}
